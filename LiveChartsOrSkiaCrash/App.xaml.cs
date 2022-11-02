69b44a4b2a31d383e27ff8f4bef0d60478f4e5e1
Igor <Igor@DESKTOP-QUFNFB5>
2022-11-02 20:38:42 +0200

    Add project files.

---
+++ LiveChartsOrSkiaCrash/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace LiveChartsOrSkiaCrash;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ LiveChartsOrSkiaCrash/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace LiveChartsOrSkiaCrash;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
