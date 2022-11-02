b023c42833a20677694d650bec0b8760b4381b12
Igor Kravchenko <igorkr.10@gmail.com>
2022-11-02 21:36:57 +0200

    Changed LiveCharts to SkiaSharp.Views.Maui.Controls

--- LiveChartsOrSkiaCrash/MauiProgram.cs
+++ LiveChartsOrSkiaCrash/MauiProgram.cs
@@ -10,7 +10,6 @@ public static class MauiProgram
 		var builder = MauiApp.CreateBuilder();
 		builder
 			.UseMauiApp<App>()
-            .UseSkiaSharp(true)
             .ConfigureFonts(fonts =>
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
